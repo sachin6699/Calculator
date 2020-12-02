f0a3599be1a49a3808464561248b391dc4ba95e5
Sachin <sachin.patel9966@gmail.com>
2020-12-02 23:12:51 +0000

    inputting values works

--- main.js
+++ main.js
@@ -1,26 +1,38 @@
 // 1. object of stored data
 const calculator = {
-    displayValue = '0',
+    displayValue: '0',
     firstOperand: null, // store first operand data
     waitingForSecondOperand: false, // check if both first and second operator have been inputted
     operator: null, // store operator for an experssion
 }
 
-// 2. function to update display
+
+// 2. input numbers into the textbox
+
+const inputDigit = (digit) => {
+  const { displayValue } = calculator;
+  //now overwrite the displayValue
+  calculator.displayValue = displayValue === '0' ? digit : displayValue + digit; // ternary operator used to check if the current value displayed on the calc is zero, if so the calc.displayValue overwrites with what digit was clicked
+}
+
+// 3. function to update display
 const updateDisplay = () => {
-    const display = document.querySelector('.textview');
+    const display = document.querySelector('.calculator-screen');
     display.value = calculator.displayValue;
 }
 updateDisplay()
 
 const keys = document.querySelector('.calculator-keys');
 keys.addEventListener('click', (event) => {
+
   const { target } = event; // same as const target = event.target;
+  
+
   if (!target.matches('button')) {
     return;
   }
 
-  if (target.classList.contains('operator')) {
+  if (target.classList.contains('operator')) {  //if classlist contains operator, log the operator and the value
     console.log('operator', target.value);
     return;
   }
@@ -35,5 +47,13 @@ keys.addEventListener('click', (event) => {
     return;
   }
 
-  console.log('digit', target.value);
+  inputDigit(target.value);
+  updateDisplay();
 });
+
+
+
+
+
+
+
